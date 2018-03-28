9e8b11a08fd9c226abdfa6c225f2ba15c9d48be0
maxuepeng <maxuepeng@torinosrc.com>
2018-03-28 22:50:49 +0800

    ishare:修复正则表达式bug

--- ebooksearch/spiders/ishare.py
+++ ebooksearch/spiders/ishare.py
@@ -17,7 +17,7 @@ class IshareSpider(scrapy.Spider):
         all_urls = [parse.urljoin(response.url,url) for url in all_urls]
         all_urls = filter(lambda x: True if x.startswith("http") else False, all_urls)
         for url in all_urls:
-            match_obj = re.match(r'(.*iask.sina.com.cn/c/(/d+).html$)', url)  # 'http://ishare.iask.sina.com.cn/c/9001.html'
+            match_obj = re.match(r'(.*iask.sina.com.cn/c/(\d+).html$)', url)  # 'http://ishare.iask.sina.com.cn/c/9001.html'
             if match_obj:
                 # 如果匹配到url，进行详情页提取
                 yield scrapy.Request(url=url, headers=self.headers, callback=self.category_parse)
