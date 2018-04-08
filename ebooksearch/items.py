875c1ce317b347d9fcbce92077d16e658252389d
maxuepeng <maxuepeng@torinosrc.com>
2018-04-08 14:03:27 +0800

    ishare:将数据插入数据库

--- ebooksearch/items.py
+++ ebooksearch/items.py
@@ -6,6 +6,8 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from scrapy.loader import ItemLoader
 
 
 class EbooksearchItem(scrapy.Item):
@@ -14,9 +16,14 @@ class EbooksearchItem(scrapy.Item):
     pass
 
 
-#爱问分享资料
-class IshareItem(scrapy.Item):
+class IshareItemLoader(ItemLoader):
+    # 自定义itemLoader
+    default_output_processor = TakeFirst()
 
+
+# 爱问分享资料
+class IshareItem(scrapy.Item):
+    # 自定义一个item给pipelines
     url_obj_id = scrapy.Field()
     title = scrapy.Field()
     upload_people = scrapy.Field()
@@ -31,3 +38,18 @@ class IshareItem(scrapy.Item):
     comment_num = scrapy.Field()
     read_num = scrapy.Field()
     collect_num = scrapy.Field()
+
+    def get_insert_sql(self):
+        insert_sql = """
+            insert into `ishare` (url_obj_id, title, upload_people, score, load_num, read_num, comment_num, collect_num, upload_time, crawl_time, url, source_website, type, size) 
+              VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE title=VALUES(title), load_num=VALUES(load_num),
+              score=VALUES(score),read_num=VALUES(read_num),comment_num=VALUES(comment_num),collect_num=VALUES(collect_num), crawl_time=VALUES(crawl_time),
+              type=VALUES(type)
+        """
+
+        params = (self["url_obj_id"], self["title"], self["upload_people"], self["score"],
+                  self["load_num"], self["read_num"], self["comment_num"], self["collect_num"],
+                  self["upload_time"], self["crawl_time"], self["url"], self["source_website"],
+                  self["type"], self["size"])
+
+        return insert_sql, params
